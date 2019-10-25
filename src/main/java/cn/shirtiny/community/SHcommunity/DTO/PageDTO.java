e561684da9cca1caa0b4e4ce0be508acc73e712e
Shirtiny <1849468240@qq.com>
2019-10-25 17:36:23 +0800

    改善面包屑导航逻辑，分为js版本与java版本

--- src/main/java/cn/shirtiny/community/SHcommunity/DTO/PageDTO.java
+++ src/main/java/cn/shirtiny/community/SHcommunity/DTO/PageDTO.java
@@ -10,4 +10,11 @@ public class PageDTO {//存储页面信息
     //每页对应路径
     String path;
 
+    public PageDTO() {
+    }
+
+    public PageDTO(String title, String path) {
+        this.title = title;
+        this.path = path;
+    }
 }
--- src/main/java/cn/shirtiny/community/SHcommunity/Service/ServiceImpl/JsHelperServiceImpl.java
+++ src/main/java/cn/shirtiny/community/SHcommunity/Service/ServiceImpl/JsHelperServiceImpl.java
@@ -15,22 +15,22 @@ public class JsHelperServiceImpl implements IjsHelperService {
 
     //创建页码数组
     @Override
-    public long[] createPageNumArray(Long curPage,Long pages){
+    public long[] createPageNumArray(Long curPage, Long pages) {
         long pageNum = curPage - 3;
-        ShArrayQueue queue=new ShArrayQueue(7);
-        for (int i=0;i<queue.getMaxSize();i++){
-            if (pageNum>0 && pageNum<=pages){
+        ShArrayQueue queue = new ShArrayQueue(7);
+        for (int i = 0; i < queue.getMaxSize(); i++) {
+            if (pageNum > 0 && pageNum <= pages) {
                 queue.add(pageNum);
             }
             pageNum++;
         }
-        return  queue.toArray();
+        return queue.toArray();
     }
 
 
     //处理和截取面包屑数组
     @Override
-    public PageDTO[] cutBreadcrumbArray(PageDTO[] breadcrumb,PageDTO curPage) {
+    public PageDTO[] cutBreadcrumbArray(PageDTO[] breadcrumb, PageDTO curPage) {
 
         //新建栈
         Stack<PageDTO> pageStack = new Stack<>();
@@ -43,10 +43,12 @@ public class JsHelperServiceImpl implements IjsHelperService {
             //遍历数组
             for (PageDTO pageDTO : breadcrumb) {
 
-                //数组内元素入栈
-                pageStack.push(pageDTO);
+                //只要当前元素path的长度比curPage的path长度小或相等,元素就入栈
+                if (pageDTO.getPath().split("/").length <= curPage.getPath().split("/").length) {
+                    pageStack.push(pageDTO);
+                }
 
-                //根据路径，比较page与要存入的page是否相同
+                //根据路径，比较page与要存入的page相同时
                 if (curPage.getPath().equals(pageDTO.getPath())) {
 
                     //相同则表示curPage已经存在，不需要存入curPage，停止继续入栈，舍弃数组以后的元素
@@ -55,8 +57,17 @@ public class JsHelperServiceImpl implements IjsHelperService {
                     //退出数组遍历
                     break;
                 }
+
+
             }
-            //如果在session中取不到值,不做处理
+        } else {
+            //如果在session中取不到值
+            //只要当前的访问页不是主页
+            if (!curPage.getPath().equals("/")) {
+                //存入一个主页
+                PageDTO homePage = new PageDTO("主页", "/");
+                pageStack.push(homePage);
+            }
         }
 
         //在数组中不存在curPage时，存入curPage
@@ -85,6 +96,6 @@ public class JsHelperServiceImpl implements IjsHelperService {
         );
 
         //把已入栈元素转为数组，用于覆盖原数组，此处利用toArray()带参数的方法
-        return pageStack.toArray(new  PageDTO[0]);
+        return pageStack.toArray(new PageDTO[0]);
     }
 }
