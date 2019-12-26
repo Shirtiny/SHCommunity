c0eddba9d475000f37eba57fd842721dfaef9300
shirtiny <1849468240@qq.com>
2019-12-26 16:34:56 +0800

    尝试解决跨域

--- src/main/java/cn/shirtiny/community/SHcommunity/Config/MyWebMvcConfigurer.java
+++ src/main/java/cn/shirtiny/community/SHcommunity/Config/MyWebMvcConfigurer.java
@@ -67,10 +67,12 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer {
     //跨域处理 全局设置，这样就不用在每个Controller上加 @CrossOrigin(origins = "http://localhost:3000")了
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000")
+        registry.addMapping("/**").allowedOrigins("https://*.shirtiny.cn")
                 .allowedHeaders("*")
                 .allowedMethods("*")
                 .maxAge(30_000);
 
     }
+
+
 }
